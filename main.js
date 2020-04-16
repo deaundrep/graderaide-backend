affbba5e2d7f2a8c424a792483e2ff00b2027035
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-04-16 10:23:27 -0400

    done

--- main.js
+++ main.js
@@ -1,6 +1,60 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const Grade = (assignment, score) =>{
+  return{
+    assignment: assignment,
+    score: score,
+  }
+
+}
+
+const Term = (hours) =>{
+  return{
+    hours: hours,
+    grades: [],
+
+    addGrade: function(func1, func2){
+      this.grades.push(Grade(func1,func2))
+
+    }
+  }
+
+}
+
+
+const Course = (name) =>{
+return{
+  name: name,
+  terms: [],
+
+  addTerm: function(func1,func2){
+    this.terms.push(Term(func1,func2))
+  }
+}
+}
+
+const Student = (name, course, term = 1) =>{
+  return{
+    name: name,
+    course: course,
+    term: term,
+    courses: [],
+
+    addCourse: function(func1,func2){
+      this.courses.push(Course(func1,func2));
+    }
+  }
+}
+
+getAverage: function(name,index){
+  
+
+}
+
+
+
+
 
 
 /*********************************
